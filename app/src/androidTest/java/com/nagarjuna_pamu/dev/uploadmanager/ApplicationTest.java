2c3c207dd30f327879e9989cd791956162df07be
Nagarjuna pamu <p_nagarjuna@students.iitmandi.ac.in>
2015-09-02 19:27:00 +0530

    init

---
+++ app/src/androidTest/java/com/nagarjuna_pamu/dev/uploadmanager/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.nagarjuna_pamu.dev.uploadmanager;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
