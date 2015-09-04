522fafc265d3442b698c91969803fdc537d9c9b2
Nagarjuna pamu <p_nagarjuna@students.iitmandi.ac.in>
2015-09-04 18:15:06 +0530

    test done

--- app/src/androidTest/java/com/nagarjuna_pamu/dev/uploadmanager/ApplicationTest.java
+++ app/src/androidTest/java/com/nagarjuna_pamu/dev/uploadmanager/ApplicationTest.java
@@ -3,11 +3,24 @@ package com.nagarjuna_pamu.dev.uploadmanager;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import com.nagarjuna_pamu.dev.uploadmanager.utils.FileUtils;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    public void testInspection() throws Throwable {
+        System.out.println("inspection folder");
+        assertEquals(FileUtils.getInspectorDir().getName() + "/", FileUtils.INSPECTOR_DIR);
+    }
+
+
+    public void testBackup() throws Throwable {
+        System.out.println("backup folder");
+        assertEquals(FileUtils.getBackupDir().getName() + "/", FileUtils.INSPECTOR_BACKUP_DIR);
     }
 }
